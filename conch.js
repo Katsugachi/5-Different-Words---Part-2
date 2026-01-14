c9f057eb46942e20bea8a99f402ac867fac982ec
Katsugachi <adriankatsugachi@gmail.com>
2026-01-14 13:57:31 +1100

    Update conch.js

--- conch.js
+++ conch.js
@@ -1,10 +1,1 @@
-window.addEventListener('load', function() {
-    const el = document.getElementById('rendering-border');
-    if (el) {
-        el.innerText = "5 - Astronomy";
-        el.style.display = "block";
-        console.log("Text set successfully!");
-    } else {
-        console.log("Element not found!");
-    }
-});
+
